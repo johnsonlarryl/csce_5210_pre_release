789217608edf7185590699499c2c2f0a2297bc5d
Larry Johnson <johnson.larryl@gmail.com>
2024-03-20 13:53:01 -0500

    Now it is working again

--- job_scheduler/tests/test_generate_schedule.py
+++ job_scheduler/tests/test_generate_schedule.py
@@ -4,7 +4,9 @@ from job_scheduler.scheduler import JobScheduler
 def test_generate_schedule(job_scheduler: JobScheduler):
     schedule = job_scheduler.generate_schedule()
 
-    print(schedule.compute_makespan())
+    expect_make_span = 27
 
-    pass
+    actual_makespan = schedule.compute_makespan()
+
+    assert expect_make_span == actual_makespan
 
